8c550d43cfc93a7bac2295588456c3ccde8a308a
CBush88 <christopherbush88@gmail.com>
2022-08-26 15:45:00 -0400

    More robust algorithm for the morning session

--- ScheduleProject/src/java/Track.java
+++ ScheduleProject/src/java/Track.java
@@ -1,22 +1,46 @@
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Track {
 
     private final List<Speech> trackSpeeches = new ArrayList<>();
     List<Speech> input;
-    //System.out.println(time.format(DateTimeFormatter.ofPattern("h:mm a")));
 
     public Track(List<Speech> input) throws Exception{
         this.input = input;
         LocalTime time = LocalTime.of(9, 0);
         LocalTime lunch = LocalTime.of(12, 0);
         while(time.isBefore(lunch)){
-            trackSpeeches.add(input.get(0));
             int minutesToAdd = getMinutesToAdd(input.get(0));
-            time = time.plusMinutes(minutesToAdd);
-            input.remove(0);
+            if(minutesToAdd == 45 &&
+                    //if only one 45 talk and the time isn't 45 or 15 after
+                    ((input.stream().filter(s -> getMinutesToAdd(s) == 45).count() == 1
+                    && !(time.getMinute() == 45 || time.getMinute() == 15))
+                    //or if time is after 11 and a 45-minute talk won't fill exactly
+                    || (time.isAfter(LocalTime.of(10,59))
+                    && !time.equals(LocalTime.of(11,15))))) {
+                List<Speech> speeches = input.stream()
+                        .filter(s -> !(getMinutesToAdd(s) == 45))
+                        .collect(Collectors.toList());
+                trackSpeeches.add(speeches.get(0));
+                time = time.plusMinutes(getMinutesToAdd(speeches.get(0)));
+                input.remove(speeches.get(0));
+            }
+            //if time remaining is less than next biggest speech, find smaller speech that fits
+            if(time.plusMinutes(minutesToAdd).isAfter(lunch)){
+                List<Speech> speeches = input.stream()
+                        .filter(s -> getMinutesToAdd(s) == minutesToAdd)
+                        .collect(Collectors.toList());
+                trackSpeeches.add(speeches.get(0));
+                time = time.plusMinutes(getMinutesToAdd(speeches.get(0)));
+                input.remove(speeches.get(0));
+            }else {
+                trackSpeeches.add(input.get(0));
+                time = time.plusMinutes(minutesToAdd);
+                input.remove(0);
+            }
         }
         if(time.equals(lunch)){
             trackSpeeches.add(new Speech("Lunch", "60min"));
