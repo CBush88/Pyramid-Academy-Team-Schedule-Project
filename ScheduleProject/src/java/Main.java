9847ac4b5da10b85fb02c350b974ed9641a9b294
CBush88 <christopherbush88@gmail.com>
2022-08-26 13:39:50 -0400

    Window resize

--- ScheduleProject/src/java/Main.java
+++ ScheduleProject/src/java/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/schedule-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 434);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 525);
         stage.setTitle("Schedule");
         stage.setScene(scene);
         stage.show();
